89acce337ea66faf4f59c692ce14b0fa1d1c35c0
Romina <lodoloromina@gmail.com>
2022-11-28 15:24:08 +0200

    update

---
+++ src/garden/vegetable/VineVegetable.java
@@ -0,0 +1,9 @@
+package garden.vegetable;
+
+public class VineVegetable {
+    // Main Method simply outputs some data
+    public static void main(String[] args) {
+        System.out.println("I am a vegetable that grows on a vine.");
+    }
+
+}
